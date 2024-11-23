f3514cd35b8042494a4ff28ca64025e62872b5c2
Caine Merrick <cainemerrick@gmail.com>
2024-11-23 11:09:48 +0000

    simplified scraper implementation

--- documentation_scraper/document_scraper.py
+++ documentation_scraper/document_scraper.py
@@ -1,11 +1,11 @@
-from typing import List
+from typing import List, Union
 from bs4 import BeautifulSoup, Tag
 
 """
 Keep it relatively simple and only specialise for edge cases
 
 You will either eventually reach:
-    *title = <div class='titlepage'> or <div class='title'> - useful for context
+    *title = <h> - useful for context
     *code = <pre> or <code>
     *text = <p> - this may contain links <a> that are useful to store
     *image = <img>
@@ -27,102 +27,10 @@ def extract_document_content(soup:BeautifulSoup) -> Tag:
     extracts the root element of document content 
     this is always the first section element
     """
-    doc_content = soup.find(name='section')
-    return doc_content
-
-def extract_subsections(doc_content:Tag):
-    """
-    extracts the subsections of a document
-    """
-    subsections = doc_content.find_all(name='section', recursive=False)
-    if len(subsections) == 0:
-        return [doc_content]
-    return subsections
-
-def extract_subsection_content(subsection:Tag) -> dict:
-    """
-    extracts and structures the content from a section ready to be chunked
-    and processed.
-    """
-    content = {
-        'title':'',
-        'content':[]
-    }
-    recursive_content_builder(subsection, content)
-    return content
-
-def recursive_content_builder(subsection, content):
-    subsection_children = subsection.children
-    for child in subsection_children:
-        if element_is_subsection_title(child):
-            content['title'] = child.text
-        elif element_is_subsection_text(child):
-            content['content'].append(extract_text_element_content(child))
-        elif element_is_subsection_code(child):
-            content['content'].append(extract_code_element_content(child))
-        elif element_is_subsection_list(child):
-            content['content'].append(extract_list_element_content(child))
-        elif element_is_subsection_table(child):
-            content['content'].append(extract_table_element_content(child))
-        elif element_is_subsection_container(child):
-            recursive_content_builder(child, content)
-
-def element_is_subsection_container(element:Tag):
-    return element.name == 'div' and len(list(element.children)) > 0
-
-def element_is_subsection_title(element:Tag):
-    return element.name == 'div' and 'titlepage' in element.attrs['class'] 
-
-def element_is_subsection_text(element:Tag):
-    return element.name == 'p'
-
-def element_is_subsection_code(element:Tag):
-    return element.name == 'pre' or element.name == 'code'
-
-def element_is_subsection_list(element:Tag):
-    return element.name == 'ul' or element.name == 'ol'
-
-def element_is_subsection_table(element:Tag):
-    return element.name == 'table'
-
-def extract_text_element_content(element:Tag) -> dict:
-    #TODO: handle links
-    text = element.text
-    if text == '':
-        return 
-    else:
-        return {
-            'type':'text',
-            'data':text
-        }
-
-def extract_code_element_content(element:Tag) -> dict:
-    return {
-        'type':'code',
-        'data':element.text
-    }
-
-def extract_list_element_content(element:Tag) -> dict:
-    element_content = {'type':'list', 'data':[]}
-    for child in element.children:
-        #TODO: extract sublists with a structure
-        if child.name == 'li':
-            element_content['data'].append(child.text)
-    return element_content
-
-def extract_table_element_content(element:Tag)-> dict:
-    element_content = {'type':'table', 'data':{}}
-    for child in element.children:
-        if child.name == 'thead':
-            extract_table_head(child, element_content)
-        elif child.name == 'tbody':
-            extract_table_body(child, element_content)
-
-def extract_table_head(element:Tag, element_content:dict)->dict:
-    pass
-
-def extract_table_body(element:Tag, element_content:dict)->dict:
-    pass
+    documentation_section = soup.find(name='section')
+    page_content = []
+    ...
+    return page_content
 
 def table_tag_to_array(table:Tag) -> list:
     table_data = []
@@ -131,10 +39,63 @@ def table_tag_to_array(table:Tag) -> list:
         table_data.append([td.text.strip().replace('\'', '') for td in row.children])
     return table_data
     
+#simple implementation
+def iterate_over_element_children(element:Tag, page_content:list):
+    
+    for child in element.children:
+        _type, extractor = identify_element_type(child)
 
+        #recursive case
+        if _type == 'container':
+            iterate_over_element_children(child, page_content)
+        #dont care case
+        elif _type is None:
+            continue
+        #content case
+        else:
+            element_content = extractor(element)
+            page_content.append(element_content)
+    
+    return page_content
+
+def identify_element_type(element:Tag) -> tuple[str, function]:
+    """
+    identifies the elements type and returns the appropiate 
+    extraction function
+    """
+    if element.name in ('div', 'section'):
+        return ('container', None)
+    elif element.name in ('ul', 'ol'):
+        return ('list', extract_list)
+    elif element.name == 'p':
+        return ('text', extract_text)
+    elif element.name in ('h1', 'h2', 'h3', 'h4', 'h5'):
+        return ('title', extract_text)
+    elif element.name == 'img':
+        return ('image', extract_image)
+    elif element.name in ('pre', 'code'):
+        return ('code', extract_text)
+    elif element.name == 'table':
+        return ('table', table_tag_to_array)
+    else:
+        return (None, None)
+
+
+#extraction functions
+def extract_text(element:Tag) -> str:
+    return element.text
+
+def extract_list(element:Tag)->List[str]:
+    element_content = []
+    for child in element.children:
+        #TODO: extract sublists with a structure
+        element_content.append(extract_text(child))
+    return element_content
+
+def extract_image(element:Tag):
+    return {'description':element.attrs['alt'], 'src':element.attrs['src']}
 
 #pql examples
-
 def extract_pql_example(table:Tag) -> dict:
     """
     pql examples are structured in a weird table format. They're very useful so they need
